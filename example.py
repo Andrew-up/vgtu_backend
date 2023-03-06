2049f4c6fdc9c8d4d50fea1a58e48e997c8756b9
andrew-up <aageew@bk.ru>
2023-03-07 00:13:31 +0300

    upd

---
+++ example.py
@@ -0,0 +1,19 @@
+
+
+add_patient_history = {
+      "patient_id": 1,
+      "comment": "test-comment",
+      "date": "test_date",
+      "history_neutral_network": {
+         "photo_original": "dddd",
+         "photo_predict": "sdfsdfsdf",
+         "annotations": [
+            {
+               "area": 264.0,
+               "bbox": "213,123,123,33",
+               "segmentation": "[1,2,3,4,5,6,11,22,fff]",
+               "category_id": 2
+            }
+         ]
+      }
+}
