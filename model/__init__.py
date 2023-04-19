40dc173f01a81e01efd3674d86822c6311e0467f
andrew-up <aageew@bk.ru>
2023-04-19 20:10:09 +0300

    upd

--- model/__init__.py
+++ model/__init__.py
@@ -1,7 +1,10 @@
-import os, sys, inspect
-# Use this if you want to include modules from a subfolder
-cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
+# pylint: disable=missing-module-docstring
+import os
+import sys
+import inspect
+
+# realpath() will make your script run, even if you symlink it :)
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(
+                        inspect.getfile(inspect.currentframe()))[0]))
 if cmd_folder not in sys.path:
     sys.path.insert(0, cmd_folder)
-
-print(sys.path)
