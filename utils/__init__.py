dfb2d62b73c7826e9b2f3c470b846226bacd264f
Andrew-up <67470168+Andrew-up@users.noreply.github.com>
2023-04-19 11:55:42 +0300

    upd2

--- utils/__init__.py
+++ utils/__init__.py
@@ -7,4 +7,4 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from model.model import HealingHistory, ResultPredict, HistoryNeuralNetwork, Annotations, Patient
 from repository.PatientRepository import PatientRepository
 from repository.ResultPredictRepository import ResultPredictRepository
-import datetime
+
