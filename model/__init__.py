80cced5f7c83fcd3e69c6e6de2be4b1569827cea
andrew-up <aageew@bk.ru>
2023-04-19 19:56:37 +0300

    upd

---
+++ model/__init__.py
@@ -0,0 +1,7 @@
+import os, sys, inspect
+# Use this if you want to include modules from a subfolder
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+
+print(sys.path)
--- utils/generateRandomData.py
+++ utils/generateRandomData.py
@@ -1,10 +1,4 @@
-import os, sys, inspect
-# Use this if you want to include modules from a subfolder
-cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe()))[0]))
-if cmd_folder not in sys.path:
-    sys.path.insert(0, cmd_folder)
 
-print(sys.path)
 
 from model.model import HealingHistory, ResultPredict, HistoryNeuralNetwork, Annotations, Patient
 from repository.PatientRepository import PatientRepository
